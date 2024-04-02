935f7d8a33f33495c0553ed21e10b0ad9f3e064c
Stepanova Diana <stepanova.diana26092002@gmail.com>
2024-04-02 17:16:34 +0300

    init

---
+++ src/component/diseace/disease.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from "../button/button";
+import  './disease.css'
+const Disease = ({disease,className, onAdd}) => {
+    const onAddHandler = () => {// в будущем будет переходить на новую страницу
+        onAdd(disease);
+    }
+    return (
+        <div className={'disease' + className}>
+            <div className={'title'}>{disease.title}</div>
+            <Button className={'add-bth'} onClick={onAddHandler}>
+                Выбрать
+            </Button>//создать другой вид кнопки для нажатия
+
+        </div>
+    );
+};
+
+export default Disease;
--- src/testList/testList.jsx
+++ src/testList/testList.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
 import './testList.css';
+import Disease from "../component/diseace/disease";
+import Button from "../component/button/button";
+
+const diseases= [
+    {id: '1', title: 'Бронхиальная астма'},
+    {id: '2', title: 'Пневмония'},
+    {id: '3', title: 'Менинговая инфекция '},
+    {id: '4', title: 'Нарушение мочеиспускания '},
+]
 const  TestList = (props) => {
 
 
     return (
-        <div >
-            TestList
+        <div  className={'list'}>
+            {diseases.map(item=> (
+                <Disease
+                    disease={item}
+                   // onAdd={onNext}
+                    className={'item'}
+                />
+            ))}
+
         </div>
     );
 
